e47c9a5ca823791f126755cdef726bf504ed43a0
ZjHOU <z@zjh.im>
2019-04-02 22:11:14 +0800

    reformat

--- src/app/js/basic-cmd/fetchNote.js
+++ src/app/js/basic-cmd/fetchNote.js
@@ -10,7 +10,7 @@ const desc = 'fetchnote - 获取 github 仓库 zjhou/notes/_posts 下的文件';
 const doc = docTpl('fetchnote', 'fetchnote <filePath>', desc);
 export default {
   doc,
-  async handler(paramObj, cmdSet, $terminal, {restParams: noteName, fromPipe}) {
+  async handler(paramObj, cmdSet, $terminal, {restParams: noteName}) {
     const noteURL = `https://api.github.com/repos/zjhou/notes/contents/_posts/${noteName}`;
     const encodedNote = await req(
       noteURL,
