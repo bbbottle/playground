6d0bc592cc7c0db54982e8c5882d89ae9743ebd6
ZjHOU <z@zjh.im>
2019-04-20 22:00:25 +0800

    refactor: 笔记相关逻辑移到 notebook 模块中

--- src/app/js/basic-cmd/fetchNote.js
+++ src/app/js/basic-cmd/fetchNote.js
@@ -1,8 +1,5 @@
-import {docTpl, req, store} from '@zhoujiahao/utils';
-
-const atou = (str) => {
-  return decodeURIComponent(escape(window.atob(str)));
-};
+import {docTpl} from '@zhoujiahao/utils';
+import {fetchNoteContent} from '@zhoujiahao/notebook'
 
 const desc = 'fetchnote - 获取 github 仓库 zjhou/notes/_posts 下的文件';
 
@@ -10,21 +7,6 @@ const doc = docTpl('fetchnote', 'fetchnote <filePath>', desc);
 export default {
   doc,
   async handler(paramObj, cmdSet, $terminal, {restParams: noteName}) {
-    const notesURL = 'https://api.github.com/repos/zjhou/notes/contents/_posts';
-    let notes = store.get('notes');
-    if (!notes) {
-      notes = await req(notesURL, 'GET');
-      store.set('notes', notes);
-    }
-    const notesNameArr = notes.map(({name}) => name.toLowerCase());
-    const targetNote = notesNameArr.filter(candi => {
-      return candi.includes(noteName.toLowerCase());
-    });
-    const noteURL = `${notesURL}/${targetNote}`;
-    const encodedNote = await req(
-      noteURL,
-      'GET'
-    );
-    return atou(encodedNote.content);
+    return fetchNoteContent(noteName);
   }
 };
