f5556bc7dba7d88d3364c21032af962d1e18bebe
ZjHOU <z@zjh.im>
2019-04-27 17:28:13 +0800

    test optmization

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -1,17 +1,40 @@
 const path = require('path');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 
+const plugins = [
+  new CleanWebpackPlugin([
+    'dist/assets',
+  ]),
+
+  new HtmlWebpackPlugin({
+    template: 'app/tpl/index.html',
+    filename: '../index.html',
+    chunks: ['main-vendor', 'main'],
+    chunksSortMode: 'dependency'
+  }),
+  new MiniCssExtractPlugin({
+    filename: `[name].[chunkhash:6].min.css`,
+    allChunks: true,
+  }),
+];
+
 module.exports = (env) => {
-  const isPRD = env === 'production';
+  const isPRD = env === 'production' || env === 'local-build';
   const projectPath = path.resolve(__dirname, 'app/');
-  const packagePath = isPRD
+  const packagePath = (isPRD && env !== 'local-build')
     ? /node_modules\/@zhoujiahao\/[a-z-]+\/lib/
     : /packages\/[a-z-]+\/lib/;
+
+  if (isPRD) {
+    plugins.push(
+      new BundleAnalyzerPlugin({analyzerMode: 'static', reportFilename: 'report.html'})
+    );
+  }
 
   return {
     entry: {
@@ -35,7 +58,10 @@ module.exports = (env) => {
           use: {
             loader: 'babel-loader',
             options: {
-              presets: ['@babel/preset-env'],
+              presets: [[
+                "@babel/preset-env", {
+                  "modules": false
+                }]],
               plugins: ['syntax-dynamic-import'],
             }
           }
@@ -53,30 +79,34 @@ module.exports = (env) => {
     },
     optimization: {
       minimize: isPRD,
+      usedExports: true,
+      providedExports: true,
+      sideEffects: true,
       moduleIds: 'hashed',
       mergeDuplicateChunks: true,
+      runtimeChunk: "single",
+      splitChunks: {
+        chunks: 'async',
+        minSize: 30000,
+        maxSize: 0,
+        minChunks: 1,
+        maxAsyncRequests: 5,
+        maxInitialRequests: 3,
+        automaticNameDelimiter: '~',
+        name: true,
+        cacheGroups: {
+          toys: {
+            test: packagePath,
+            priority: -10
+          },
+          default: {
+            minChunks: 2,
+            priority: -20,
+            reuseExistingChunk: true
+          }
+        }
+      }
     },
-    plugins: [
-      // 分析打包后的模块
-
-      ( isPRD || env === 'local' )
-        ? new BundleAnalyzerPlugin({analyzerMode: 'static', reportFilename: 'report.html'})
-        : null,
-
-      new CleanWebpackPlugin([
-        'dist/assets',
-      ]),
-
-      new HtmlWebpackPlugin({
-        template: 'app/tpl/index.html',
-        filename: '../index.html',
-        chunks: ['main-vendor', 'main'],
-        chunksSortMode: 'dependency'
-      }),
-      new MiniCssExtractPlugin({
-        filename: `[name].[chunkhash:6].min.css`,
-        allChunks: true,
-      }),
-    ],
+    plugins
   }
 };
