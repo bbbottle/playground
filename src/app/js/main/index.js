2ac564a18913824ff5f3d56df091574e4f81aeac
ZjHOU <z@zjh.im>
2019-05-12 16:55:51 +0800

    fix: style

--- src/app/js/main/index.js
+++ src/app/js/main/index.js
@@ -1,7 +1,7 @@
 import {$} from '@zhoujiahao/utils';
 import installCommands from './preInstall';
 
-import '../../style/index.scss';
+import 'style/index.scss';
 const init = async () => {
   const {default: PseudoTerminal} = await import('@zhoujiahao/terminal');
   const Terminal = PseudoTerminal($('#terminal'));
