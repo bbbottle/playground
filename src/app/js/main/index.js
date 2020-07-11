efb7fefc2fcff838aa4c860fd032a2fc9f0b695d
ZjHOU <z@zjh.im>
2020-07-11 17:52:52 +0800

    fix: 暂时只保留博客

--- src/app/js/main/index.js
+++ src/app/js/main/index.js
@@ -1,51 +1,11 @@
-import {$, isMobile} from '@zhoujiahao/utils';
-import preInstall from './preInstall';
-
 import 'style/index.scss';
+import 'style/markdown.css';
+
 const init = async () => {
-  const {default: PseudoTerminal} = await import('@zhoujiahao/terminal');
-  const Terminal = PseudoTerminal($('#terminal'), {
-    // background: 'rgba(253, 165, 142, 0.8784313725490196)',
-    // color: '#fff',
-    // cursorColor: '#fff',
-  });
-  window.Terminal = Terminal;
-
-  document.addEventListener('click', function (evt) {
-    let isCommand = Array.from(evt.target.classList).includes('command');
-    if (!isCommand) return;
-
-    let command = evt.target.getAttribute('data-cmd');
-    let isMulti = evt.target.hasAttribute('multi');
-    let toExec = isMulti ? command.split(/\s*&&\s*/) : command;
-    let execFn = isMulti
-      ? 'humanizerExecCmdArr'
-      : 'humanizerExec';
-
-    evt.target.classList.remove('command');
-    Terminal[execFn](toExec).then(() => {
-      evt.target.classList.add('command');
-    });
-  });
-
-  Terminal.addCommands({
-    hi: (p1 = {}, p2, p3, p4, print) => {
-      print('This site is currently constructing...')
-      print('comming soon!')
-      return;
-    }
-  })
-
-  Terminal.addCommands({preInstall});
-  await Terminal.humanizerExec('preInstall');
-
-  if (isMobile()) {
-    return Terminal.humanizerExec('hi');
-  }
+  const { default: blogMod } = await import('@zhoujiahao/blog');
+  return blogMod.handler();
 };
 
 init().then(() => {
-  if (!isMobile()) {
-    $(".commands").classList.add('show');
-  }
+  console.log('loaded');
 });
