df602ec32a4179f07047dc3b969c111ad3ca48e1
bbbottle <z@zjh.im>
2020-11-20 23:03:55 +0800

    chore: install from github registry

--- src/app/js/components/page_comp/pages.jsx
+++ src/app/js/components/page_comp/pages.jsx
@@ -2,46 +2,29 @@ import React from 'react';
 
 import {
   RulerIcon,
-  EditIcon,
   PostIcon,
   AboutIcon,
   PhotoIcon,
-  QuestionIcon
 } from '@zhoujiahao/bblego';
 import { isMobile, pick } from "@zhoujiahao/utils";
 
 import { Columns } from '../util_comp/layout';
 import { Design } from './design';
-import { Editor } from './editor';
 import { CanvasPage } from './canvas';
 import { Posts } from './post_list'
 import { About } from './about';
 import { Photos } from "./photos";
 import { CursorContext } from '../util_comp/cursor';
-// import { Unknown } from "./unknown";
 
 const IS_MOBILE = isMobile();
 
 export const pagesBuilder = (tools) => {
-  const WrappedEditor = () => {
-    const openMenu = () => {};
-    const gotoPage = () => {};
-    return (
-      <Editor openMenu={openMenu} gotoPage={gotoPage} />
-    )
-  };
 
   const pages = {
     design: {
       title: '美术部',
       icon: <RulerIcon />,
       component: <Design />,
-    },
-    editor: {
-      title: '编辑部',
-      icon: <EditIcon />,
-      fullScreen: true,
-      component: <WrappedEditor />,
     },
     posts: {
       title: '杂记',
@@ -73,19 +56,12 @@ export const pagesBuilder = (tools) => {
       icon: <RulerIcon />,
       component: <CanvasPage />
     },
-    // unknown: {
-    //   title: '未知',
-    //   icon: <QuestionIcon />,
-    //   component: <Unknown />
-    // }
   };
 
   const PcPages = pick(pages, [
-    // 'design',
-    // 'editor',
+    'canvas',
     'articles',
     'about',
-    'canvas',
   ]);
 
   const MobilePages = pick(pages, [
