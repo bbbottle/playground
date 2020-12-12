ac05ecf12448ed04cb4c4f597a92fd8c559fd829
bbbottle <z@zjh.im>
2020-12-13 01:50:31 +0800

    chore: add manifest and listen to install event

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,3 +3,10 @@ import { renderZjhDotIm } from '@bbbottle/zjh.im';
 renderZjhDotIm(
   document.getElementById('gui')
 );
+
+window.addEventListener('beforeinstallprompt', (event) => {
+  console.log('👍', 'beforeinstallprompt', event);
+  event.userChoice.then((result) => {
+    console.log('👍', 'userChoice', result);
+  });
+});
