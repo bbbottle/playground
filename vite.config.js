9b25d900d56d1a1174be889be186545824bf9a51
zjhou <z@zjh.im>
2021-05-13 21:14:22 +0800

    reformat

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
-import rollupOptions from './rollup.config';
+import rollupOptions from "./rollup.config";
 
-const PROD = 'production';
+const PROD = "production";
 
 const externalDependencies = {
-  "rxjs": "6.6.3",
+  rxjs: "6.6.3",
   "rxjs/operators": "6.6.3",
-  "classnames": "2.2.6",
-  "immer": "8.0.1",
-  "react": "17.0.1",
+  classnames: "2.2.6",
+  immer: "8.0.1",
+  react: "17.0.1",
   "react-dom": "17.0.1",
   "prop-types": "15.7.2",
 };
@@ -16,31 +16,31 @@ const externalsDepsToEsmDotShCDNURL = (depVerMap = {}) => {
   const baseCDNURL = "https://esm.sh";
   const retMap = {};
 
-  for(const dep in depVerMap) {
-    const [d, ...rest] = dep.split('/');
-    const suffixPath = rest.join('/');
+  for (const dep in depVerMap) {
+    const [d, ...rest] = dep.split("/");
+    const suffixPath = rest.join("/");
     const ver = depVerMap[d];
     const pkg = `${d}@${ver}`;
     retMap[dep] = `${baseCDNURL}/${pkg}/${suffixPath}`;
   }
 
   return retMap;
-}
+};
 
 export default ({ mode }) => {
   const alias = externalsDepsToEsmDotShCDNURL(externalDependencies);
 
   const sharedOptions = {
-    root: 'src',
+    root: "src",
     json: { stringify: true },
     resolve: { alias },
   };
 
   const buildOptions = {
     build: {
-      outDir: '../dist',
-      name: 'index.js',
-      target: 'esnext',
+      outDir: "../dist",
+      name: "index.js",
+      target: "esnext",
       rollupOptions,
     },
   };
@@ -48,19 +48,15 @@ export default ({ mode }) => {
   const serveOptions = {
     server: {
       port: 8080,
-    }
+    },
   };
 
   const isBuilding = mode === PROD;
 
-  console.log('----', isBuilding);
+  console.log("----", isBuilding);
 
   return {
     ...sharedOptions,
-    ...(
-      isBuilding
-        ? buildOptions
-        : serveOptions
-    )
-  }
-}
+    ...(isBuilding ? buildOptions : serveOptions),
+  };
+};
